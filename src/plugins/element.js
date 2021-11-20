1011fea90060c33e20a982965e94753497a04eeb
Melancholy <1348284787@qq.com>
2021-11-20 15:43:35 +0800

    完成了订单功能的开发

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import { Button,Form,FormItem,Input,Message,Container,Header,Aside,Main,Menu,Submenu,MenuItemGroup,MenuItem,Breadcrumb,
   BreadcrumbItem,Card,Row,Col,Table,TableColumn,Switch,Tooltip,Pagination,Dialog,MessageBox,Tag,Tree,Select,Option,Cascader,
   Alert,Tabs,TabPane,Steps,Step,Checkbox,CheckboxGroup,Upload} from 'element-ui'
+import {Timeline} from "element-ui";
+import {TimelineItem} from "element-ui";
 
 Vue.use(Button)
 Vue.use(Form)
@@ -39,6 +41,8 @@ Vue.use(Step)
 Vue.use(Checkbox)
 Vue.use(CheckboxGroup)
 Vue.use(Upload)
+Vue.use(Timeline)
+Vue.use(TimelineItem)
 
 Vue.prototype.$message = Message
 Vue.prototype.$confirm = MessageBox.confirm
